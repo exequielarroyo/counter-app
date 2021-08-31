17e5da5cab501a3e704a424dcda5ae265f82ff6f
Zekiel <zekielarroyo@gmail.com>
2021-08-31 21:21:10 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div>
+			<button className="btn" id="sub">-</button>
+      <span>0</span>
+			<button className="btn" id="add">+</button>
+		</div>
+	);
+}
+
+export default App;
